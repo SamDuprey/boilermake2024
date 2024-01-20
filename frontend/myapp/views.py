cdd56be0749f8ec95220ea1484055443496cc4c7
sduprey <sam.d.duprey@gmail.com>
2024-01-20 14:29:48 -0500

    cleaned up unnecessary code and added displaying video

--- frontend/myapp/views.py
+++ frontend/myapp/views.py
@@ -1,7 +1,4 @@
 from django.shortcuts import render, HttpResponse
-from .models import TodoItem
-from .models import Project
-from .models import MyForm
 from .models import *
 import pandas as pd
 from plotly.offline import plot
@@ -25,34 +22,3 @@ def home(request):
 
 def create(request):
     return render(request, "create.html")
-
-def todos(request):
-    items = TodoItem.objects.all()
-    return render(request, "todos.html", {"todos": items})
-
-def contentForm(request):
-    if request.method == 'POST':
-        form = MyForm(request.POST)
-        if form.is_valid():
-            dropdown_value = form.cleaned_data['dropdown_field']
-            
-            # Pass the value to your other Python script
-            result = dropdown_value
-            print(result)
-            
-            return render(request, 'result.html', {'result': result})
-    else:
-        form = MyForm()
-
-    return render(request, 'create.html', {'form': form})
-
-def dropdownsearch(request):
-    if request.method=="POST":
-        searchgender=request.POST.get('gender')
-        searchdesignation=request.POST.get('designation')
-        empsearch=Employee.objects.filter(gender=searchgender,designation=searchdesignation)
-        return render(request,'home.html',{"data":empsearch})
-    else:
-        displayemp=Employee.objects.all()
-
-        return render(request,'home.html',{"data":displayemp})
