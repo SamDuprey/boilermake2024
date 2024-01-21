5b824db57eeaf3d925fffa55f79c308164bbf12f
sduprey <sam.d.duprey@gmail.com>
2024-01-21 00:40:16 -0500

    set up real reddit stories

--- frontend/myapp/urls.py
+++ frontend/myapp/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 from . import views
-from .views import execute_dummy
+from .views import *
 
 urlpatterns = [
     path('execute_dummy/', execute_dummy, name='execute_dummy'),
+    path('execute_real/', execute_real, name='execute_real'),
     path("", views.home, name="home"),
     path("create/", views.create, name="create"),
-    # path("options/", views.options, name="options"),
     path("story_options/", views.story_options, name="story_options"),
     path("ai/", views.ai, name="ai"),
     path("history/", views.history, name="history"),
