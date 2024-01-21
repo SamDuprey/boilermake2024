f04e541d14a994a2ce30a3c7d345081a842a5068
sduprey <sam.d.duprey@gmail.com>
2024-01-21 00:57:54 -0500

    added topic dropdown function calling for history

--- frontend/myapp/urls.py
+++ frontend/myapp/urls.py
@@ -5,6 +5,8 @@ from .views import *
 urlpatterns = [
     path('execute_dummy/', execute_dummy, name='execute_dummy'),
     path('execute_real/', execute_real, name='execute_real'),
+    path('execute_history/', execute_history, name='execute_history'),
+    path('execute_history_custom/', execute_history_custom, name='execute_history_custom'),
     path("", views.home, name="home"),
     path("create/", views.create, name="create"),
     path("story_options/", views.story_options, name="story_options"),
