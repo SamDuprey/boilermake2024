5bdea9e6a47104be19275431d2d37960b5aec115
Colin Wu <85907102+ColinWu0403@users.noreply.github.com>
2024-02-05 20:50:41 -0500

    Added .env to project for API keys, cleaned up code

---
+++ Dockerfile
@@ -0,0 +1,2 @@
+# Use an official Python image as the base image
+FROM python:3.11.7
--- frontend/scripts/test_openai.py
+++ frontend/scripts/test_openai.py
@@ -1,11 +1,12 @@
 import os
 from openai import OpenAI
+from decouple import config
 import asyncio
 def generate_story(subreddit):
-    client = OpenAI(
-        # This is the default and can be omitted
-        api_key='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',
-    )
+  
+    # gets the API key from the .env file
+    OPENAI_API_KEY = config('OPENAI_API_KEY')
+    client = OpenAI(api_key=OPENAI_API_KEY)
 
     user_message = f"Create a short example post that replicates the nature of stories in the subreddit r/{subreddit}. Make the post short, under 200 words. The story should contain crazy, scanalous topics that create discussion. But the narrator shouldn't ever directly say that the story is contraversial, as it is more effective to show than tell. In fact, avoid sounding corny at all costs, and do NOT say the phrase 'buckle up'"
 
